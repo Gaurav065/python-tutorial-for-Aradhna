3d7d07e87d449816e20029d80e8421c7f59a119b
gaurav065 <warrior.prince652002@gmail.com>
2025-04-29 10:28:00 +0530

    varaible tutorial

---
+++ Basic of python/Variables.py
@@ -0,0 +1,16 @@
+message = "Hello, this is the second program that I am writing in Python"   
+
+message="Hello, this is the  second message that I am writing in Python"
+
+
+
+# letters, numbers, underscore
+# the variable name should always start with a Letter or an underscore
+
+a = 1
+
+
+b = float (a)
+print(type(b))
+
+print(message)
